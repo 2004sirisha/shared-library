503c3c8dd17577e6f6350ce062c0bb6e6fce73af
2004sirisha <159569463+2004sirisha@users.noreply.github.com>
2024-02-10 00:15:13 +0530

    Update arjun.groovy

--- vars/arjun.groovy
+++ vars/arjun.groovy
@@ -1,5 +1,7 @@
 def call (Map params) {
  def branch_name = params.branch_name
+def project_name = params.project_name
+def base_url = params.base_url
  pipeline {
     agent any
 
@@ -7,7 +9,7 @@ def call (Map params) {
         stage('Hello') {
             steps {
                 echo 'Hello World'
-                 echo branch_name
+                echo base_url/project_name
             }
         }
         stage('build') {
