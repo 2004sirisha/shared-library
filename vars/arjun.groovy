a9d3c5aca20decfdd9adb83bad65b68cb3647b38
2004sirisha <159569463+2004sirisha@users.noreply.github.com>
2024-02-10 01:16:42 +0530

    Update arjun.groovy

--- vars/arjun.groovy
+++ vars/arjun.groovy
@@ -5,10 +5,8 @@ def call(String branch_name, String project_name, String base_url) {
         stages {
             stage('Hello') {
                 steps {
-                    script {
                         echo 'Hello World'
-                        echo "${base_url}/${project_name}/${branch_name}" // Corrected string interpolation syntax
-                    }
+                        echo "${base_url}/${project_name}/${branch_name}" // Corrected string interpolation synt
                 }
             }
             stage('build') {
