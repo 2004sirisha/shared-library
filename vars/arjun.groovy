2549a792823dc537929d3accbc01efe792453506
2004sirisha <159569463+2004sirisha@users.noreply.github.com>
2024-02-10 00:36:07 +0530

    Update arjun.groovy

--- vars/arjun.groovy
+++ vars/arjun.groovy
@@ -1,22 +1,23 @@
-def call (Map params) {
-def branch_name = params.branch_name
-def project_name = params.project_name
-def base_url = params.base_url
- pipeline {
-    agent any
+def call(Map params) {
+    def branch_name = params.branch_name
+    def project_name = params.project_name
+    def base_url = params.base_url
+    
+    pipeline {
+        agent any
 
-    stages {
-        stage('Hello') {
-            steps {
-                echo 'Hello World'
-                echo base_url/project_name
+        stages {
+            stage('Hello') {
+                steps {
+                    echo 'Hello World'
+                    echo "${base_url}/${project_name}" // Corrected string interpolation syntax
+                }
             }
-        }
-        stage('build') {
-            steps {
-                echo 'test build'
+            stage('build') {
+                steps {
+                    echo 'test build'
+                }
             }
         }
     }
-}
 }
