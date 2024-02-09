53c87632e0d47f8137bba11a47dd933096fa379e
2004sirisha <159569463+2004sirisha@users.noreply.github.com>
2024-02-09 23:30:57 +0530

    Update arjun.groovy

--- vars/arjun.groovy
+++ vars/arjun.groovy
@@ -1,1 +1,14 @@
+def call () {
+   pipeline {
+      agent any
 
+      stages {
+          stage('Hello') {
+              steps {
+                  echo 'Hello World'
+            }
+        }
+    }
+}
+
+}
